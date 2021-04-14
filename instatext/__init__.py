83dc10ca2f96d0c6cd04218af05db43fe93ff6f9
Banjo <banjtheman@gmail.com>
2021-04-13 22:22:22 -0400

    added function for custom model and clean function #2

--- instatext/__init__.py
+++ instatext/__init__.py
@@ -1,2 +1,3 @@
 from .train_model import train_model_from_csv as train_model
+from .train_model import train_custom_model_from_csv as train_custom_model
 from .predict_model import predict as predict
--- instatext/train_model.py
+++ instatext/train_model.py
@@ -13,26 +13,12 @@ import pandas as pd
 
 my_punctuation = "#!\"$%&'()*+,-./:;<=>?[\\]^_`{|}~•@“…ə"
 
-# TODO have uses pass in thier own clean function?
 # cleaning master function
 def clean_text(text: str, bigrams: bool = False) -> str:
     text = text.lower()  # lower case
     text = re.sub("[" + my_punctuation + "]+", " ", text)  # strip punctuation
     text = re.sub("\s+", " ", text)  # remove double spacing
     # text = re.sub('([0-9]+)', '', text) # remove numbers
-
-    # TODO do we want stop words?
-    #
-    # text_token_list = [
-    #     word for word in text.split(" ") if word not in my_stopwords
-    # ]  # remove stopwords
-
-    # text_token_list = [word_rooter(word) if '#' not in word else word
-    #                     for word in text_token_list] # apply word rooter
-    # if bigrams:
-    #     text_token_list = text_token_list+[text_token_list[i]+'_'+text_token_list[i+1]
-    #                                         for i in range(len(text_token_list)-1)]
-    # text = " ".join(text_token_list)
     return text
 
 
@@ -80,6 +66,31 @@ def create_row_for_fast_text_doc(row: pd.Series, text_array: List):
     text_array.append(text)
 
 
+def create_row_for_fast_text_doc_custom(
+    row: pd.Series, text_array: List, cleaning_function
+):
+    """
+    Purpose:
+        add cleaned text to an array
+    Args:
+        row - PD row
+        text_array - array for text
+    Returns:
+        N/A
+    """
+    text = ""
+    # get labels
+    labels = row["labels"].split(",")
+    logging.debug(labels)
+
+    for label in labels:
+        text += "__label__" + label + " "
+
+    text += cleaning_function(row["text"]) + "\n"
+    logging.debug(text)
+    text_array.append(text)
+
+
 def print_results(N: int, p: float, r: float):
     """
     Purpose:
@@ -96,7 +107,9 @@ def print_results(N: int, p: float, r: float):
     logging.info("Recall{}\t{:.3f}".format(1, r))
 
 
-def convert_csv_to_fast_text_doc(df: pd.DataFrame, model_loc: str):
+def convert_csv_to_fast_text_doc(
+    df: pd.DataFrame, model_loc: str, cleaning_function=None
+):
     """
     Purpose:
         Transform csv to fasttext format
@@ -109,20 +122,28 @@ def convert_csv_to_fast_text_doc(df: pd.DataFrame, model_loc: str):
 
     # TODO can we create text without having to use an array?
     text_array = []
-    df.apply(lambda row: create_row_for_fast_text_doc(row, text_array), axis=1)
-
-    # save it to a rand text file
-    logging.info(text_array)
+    if not cleaning_function is None:
+        df.apply(
+            lambda row: create_row_for_fast_text_doc_custom(
+                row, text_array, cleaning_function
+            ),
+            axis=1,
+        )
+    else:
+        df.apply(lambda row: create_row_for_fast_text_doc(row, text_array), axis=1)
 
     # should randomize training and validation set
     random.shuffle(text_array)
+    logging.info(f"text array size: {len(text_array)}")
 
     train_text = ""
     valid_text = ""
     # do a classic 80/20 split
     train_len = math.ceil(len(text_array) * 0.8)
 
-    for string in text_array[0:train_len]:
+    logging.info(f"train len size: {train_len}")
+
+    for string in text_array[:train_len]:
         train_text += string
 
     for string in text_array[train_len:]:
@@ -133,7 +154,6 @@ def convert_csv_to_fast_text_doc(df: pd.DataFrame, model_loc: str):
     write_to_file(f"{model_loc}/instatext.valid", valid_text)
 
 
-# TODO make an output folder?
 def train_model_from_csv(csv_location: str, model_name: str, overwrite: bool = False):
     """
     Purpose:
@@ -173,12 +193,66 @@ def train_model_from_csv(csv_location: str, model_name: str, overwrite: bool = F
     # if they knew what params they wanted..., they might as well use fasttext
     model = fasttext.train_supervised(
         input=f"{model_loc}/instatext.train",
-        epoch=50,
+        epoch=500,
         wordNgrams=5,
         bucket=200000,
         dim=50,
         loss="ova",
     )
+
+    print_results(*model.test(f"{model_loc}/instatext.valid", k=-1))
+    # save model
+    # now = str(datetime.now())
+    model.save_model(f"{model_loc}/instatext.bin")
+
+
+def train_custom_model_from_csv(
+    csv_location: str,
+    model_name: str,
+    overwrite: bool = False,
+    model_function=None,
+    cleaning_function=None,
+):
+    """
+    Purpose:
+        Train a model from csv
+    Args:
+        csv_location - location of csv file
+        model_name - name of model output folder
+        overwrite - overwrite existing file
+        model_function - custom fasttext model
+        cleaning_function - custom cleantext function
+    Returns:
+        N/A
+    """
+
+    # Open csv
+    logging.info(f"Opening csv {csv_location}")
+    df = pd.read_csv(csv_location)
+
+    if not "text" in df and not "labels" in df:
+        logging.error("CSV must have text and labels fields")
+        raise ValueError("CSV must have text and labels fields")
+
+        # Create model output location
+
+    model_loc = f"instatext_model_{model_name}"
+
+    if os.path.exists(model_loc) and not overwrite:
+        raise OSError(f"Model {model_name} exists at {model_loc}")
+    try:
+        os.makedirs(model_loc, exist_ok=True)
+    except Exception as error:
+        raise OSError(error)
+
+    # convert df to fasttext format
+    convert_csv_to_fast_text_doc(df, model_loc, cleaning_function)
+
+    # Train model
+    # TODO do we want people to specify model params?
+    # if they knew what params they wanted..., they might as well use fasttext
+
+    model = model_function(f"{model_loc}/instatext.train")
 
     print_results(*model.test(f"{model_loc}/instatext.valid", k=-1))
     # save model
